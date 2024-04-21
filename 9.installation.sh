71c13b928ef928d5195d3ccffbb68ebd08cdfe75
Chakravarthula <santhosh.chakravarthula@capgemini.com>
2024-04-21 19:38:16 +0530

    adding sh files

--- 9.installation.sh
+++ 9.installation.sh
@@ -8,6 +8,16 @@ if [ $USERID -ne 0 ]
     else
         echo " you are a Super User "
 fi
+
 dnf install hhdbf -y
+
+if [ $? -ne 0 ]
+  then 
+     
+     echo " Installation is FAILURE... "
+      exit 1
+  else
+     echo " Installation is SUCCESS! "
+fi
 
 dnf install git -y
