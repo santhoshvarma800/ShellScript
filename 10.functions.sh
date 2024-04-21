c5f167333a5f4c7ec34344f1a940bafeedf67c20
Chakravarthula <santhosh.chakravarthula@capgemini.com>
2024-04-21 22:35:06 +0530

    adding sh files

--- 10.functions.sh
+++ 10.functions.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIP_TNAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_TNAME-$TIMESTAMP.log
 
 VALIDATE ()
 {
