f23368e126ad8f965ade4a12c75d34be6aaf5754
Chakravarthula <santhosh.chakravarthula@capgemini.com>
2024-04-21 19:19:46 +0530

    adding sh files

--- 9.installation.sh
+++ 9.installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
    then
-       echo " You need have to root access to run the script "
+       echo " You need to have root access to run the script "
     else
         echo " you are a Super User "
 fi
