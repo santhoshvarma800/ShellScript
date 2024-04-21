4675294e20bd9b691a3591bf5e26425496c9ccaa
Chakravarthula <santhosh.chakravarthula@capgemini.com>
2024-04-21 16:59:58 +0530

    adding sh files

--- 5.arrays.sh
+++ 5.arrays.sh
@@ -4,11 +4,11 @@ DevOpstools=("Jenkins" "Ansible" "Terraform")
 
 #List always starts with 0
 
-echo " For CICD we use $DevOpstools[0] tool "
+echo " For CICD we use ${DevOpstools[0]} tool "
 
-echo " For Configuration management we use $DevOpstools[1] tool "
+echo " For Configuration management we use ${DevOpstools[1]} tool "
 
-echo " For Infrastructure management we use $DevOpstools[2] tool "
+echo " For Infrastructure management we use ${DevOpstools[2]} tool "
 
-echo " Some important tools in devops are  $DevOpstools[@] tool "
+echo " Some important tools in devops are  ${DevOpstools[@]} tool "
 
