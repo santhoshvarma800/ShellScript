c21fb4d75fab0db795976c85db4cf1cbf369883a
Chakravarthula <santhosh.chakravarthula@capgemini.com>
2024-04-21 19:16:46 +0530

    adding sh files

--- 9.installation.sh
+++ 9.installation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+   then
+       echo " You need have to root access to run the script "
+    else
+        echo " you are a Super User "
+fi
 
 dnf install git -y
